3c3cc42a8b498051133a7fd5132cfa7bc9a167c2
chen0682 <chen0682@algonquinlive.com>
2020-10-25 18:25:16 -0400

    first save

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from 'react'
+import { data } from '../../../data'
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
-};
+  const [people, setPeople] = React.useState(data)
 
-export default UseStateArray;
+  return (
+    <React.Fragment>
+      {people.map((person) => {
+        console.log(person)
+        const { id, name } = person
+
+        const removeItem = (id) => {
+          let newPeople = people.filter((person) => person.id !== id)
+          setPeople(newPeople)
+        }
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button className='btn-sm' onClick={() => removeItem(id)}> Remove </button>
+          </div>
+        )
+      })}
+      <button className='btn' onClick={() => setPeople([])}>clear items</button>
+    </React.Fragment>
+  )
+}
+
+export default UseStateArray
