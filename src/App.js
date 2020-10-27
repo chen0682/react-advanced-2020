9d8b172f48a5802077b6af701a9e034a1244a8a5
chen0682 <chen0682@algonquinlive.com>
2020-10-27 08:35:45 -0400

    useEffect-fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/5-useState-counter'
+// import Setup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+// import Setup2 from './tutorial/1-useState/setup/2-useState-basics'
 
 function App () {
   return (
     <div className='container'>
       <Setup />
+      {/* <Setup2 /> */}
     </div>
   )
 }
